6ffce06acd3b80fdcb154a8d07a3ab3bace1b42c
Jeffrey Smith <jeffrey_s_smith@live.com>
2022-09-22 16:13:43 -0700

    lab4

---
+++ src/server.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const express = require('express');
+// const foodRouter = require('./routes/food');
+// const logger = require('./middleware/logger');
+
+
+
+const app = express();
+
+app.use(express.json());
+// app.use(logger);
+// app.use(foodRouter);
+
+
+// my error messages
+const notFound = require('./error-handlers/404.js');
+const serverError = require('./error-handlers/500.js');
+
+// server routes
+app.get('/', (request, response) => {
+  response.status(200).send('Welcome to my server');
+});
+
+
+
+// server error handling
+// app.get('*', notFound);
+// app.use(serverError);
+
+module.exports = {
+  start: (port) => {
+    app.listen(port, () => {
+      console.log('app is running!!');
+    });
+  },
+  app,
+};
+
